df397766c7da5c38f736b24d7ff97b40caefa4e5
deepakjangir94 <deepakkumar0307001@gmail.com>
2025-08-18 20:00:31 +0530

    version 1.8 everything is working fine upto this point. register, logout and login of a new user is getting reflected in this app

--- src/features/auth/authService.jsx
+++ src/features/auth/authService.jsx
@@ -4,12 +4,21 @@ const API_URL = '/api/users/';
 
 const register = async(userData) => {
     const response = await axios.post(API_URL, userData);
-    console.log('eske baad backchodi machti hai')
     if(response.data) {
         localStorage.setItem('user', JSON.stringify(response.data));
     }
     return response.data;
 }
 
-const authService = { register };
+const logout = () => localStorage.removeItem('user');
+
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData);
+    if(response.data){
+        localStorage.setItem('user', JSON.stringify(response.data));
+    }
+    return response.data;
+}
+
+const authService = { register, logout, login };
 export default authService;
